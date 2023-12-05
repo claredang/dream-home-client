32cf1153f6ea6488682c1bd933d45583c65c8ea3
Clare Dang <claredng@gmail.com>
2023-12-05 14:33:19 -0800

    change image layout for quiz result

--- src/components/PinterestLayout.js
+++ src/components/PinterestLayout.js
@@ -16,34 +16,34 @@ function PinterestLayout({ isLoop = false, images }) {
             />
             <Card
               // key={index}
-              size="small"
+              size="medium"
               src={require(`../../src/assests/quiz-image/${element}/${element}_${
                 index + 2
               }.jpg`)}
             />
             <Card
-              size="medium"
+              size="small"
               src={require(`../../src/assests/quiz-image/${element}/${element}_${
                 index + 3
               }.jpg`)}
             />
             <Card
               // key={index}
-              size="small" // You can adjust the size as needed
+              size="medium" // You can adjust the size as needed
               src={require(`../../src/assests/quiz-image/${element}/${element}_${
                 index + 4
               }.jpg`)}
             />
             <Card
               // key={index}
-              size="small"
+              size="medium"
               src={require(`../../src/assests/quiz-image/${element}/${element}_${
                 index + 5
               }.jpg`)}
             />
             <Card
               // key={index}
-              size="medium"
+              size="small"
               src={require(`../../src/assests/quiz-image/${element}/${element}_${
                 index + 6
               }.jpg`)}
